07cb94d4dfefcfe6cf23b697926776fd438cf0a1
darong <darongvann@gmail.com>
2018-08-07 16:46:13 +0700

    asdfsdf

--- src/main/java/com/eracambodia/era/configuration/datasource/EraDataSource.java
+++ src/main/java/com/eracambodia/era/configuration/datasource/EraDataSource.java
@@ -1,7 +1,5 @@
 package com.eracambodia.era.configuration.datasource;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.boot.jdbc.DataSourceBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -15,7 +13,7 @@ public class EraDataSource {
     public DataSource dataSource(){
         return DataSourceBuilder
                 .create()
-                .url("jdbc:postgresql://ec2-54-221-210-97.compute-1.amazonaws.com:5432/dfotv5hpoiu33h")
+                .url("jdbc:postgresql://ec2-54-221-210-97.compute-1.amazonaws.com:5432/dfotv5hpoiu33h?sslmode=require")
                 .username("mzvvacdhmvtzhb")
                 .password("c7980b63fb09912f6e4ac0bd0aeed0069ba6eb41e706b0bb23340e60c73d98e8")
                 .driverClassName("org.postgresql.Driver")
--- src/main/java/com/eracambodia/era/configuration/mybatis/EraMyBatis.java
+++ src/main/java/com/eracambodia/era/configuration/mybatis/EraMyBatis.java
@@ -31,14 +31,4 @@ public class EraMyBatis {
         return new DataSourceTransactionManager(dataSource);
     }
 
-    @Bean(name = "dataSource")
-    public DataSource dataSource(){
-        return DataSourceBuilder
-                .create()
-                .url("jdbc:postgresql://ec2-54-221-210-97.compute-1.amazonaws.com:5432/dfotv5hpoiu33h")
-                .username("mzvvacdhmvtzhb")
-                .password("c7980b63fb09912f6e4ac0bd0aeed0069ba6eb41e706b0bb23340e60c73d98e8")
-                .driverClassName("org.postgresql.Driver")
-                .build();
-    }
 }
