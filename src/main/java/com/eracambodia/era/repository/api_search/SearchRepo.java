260c16c11a171eeb3849f232f27727f88506cad6
darong <darongvann@gmail.com>
2018-09-07 08:50:23 +0700

    asddfd

--- src/main/java/com/eracambodia/era/repository/api_search/SearchRepo.java
+++ src/main/java/com/eracambodia/era/repository/api_search/SearchRepo.java
@@ -61,7 +61,7 @@ public interface SearchRepo {
     @Options(statementType = StatementType.CALLABLE)
     Integer countSearch(String keyword);
 
-    @Select("SELECT table_name " +
+    @Select("SELECT type " +
             "FROM building")
     List<String> projectType();
 }
