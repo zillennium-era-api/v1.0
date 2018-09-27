e46feeabc35ea3fbaba18762d8435516a1c11e76
darong <darongvann@gmail.com>
2018-09-27 16:55:56 +0700

    aaa

--- src/main/java/com/eracambodia/era/ServletInitializer.java
+++ src/main/java/com/eracambodia/era/ServletInitializer.java
@@ -3,9 +3,9 @@ package com.eracambodia.era;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
-public class ServletInitializer /*extends SpringBootServletInitializer*/ {
-   /* @Override
+public class ServletInitializer extends SpringBootServletInitializer {
+    @Override
     protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
         return builder.sources(EraApplication.class);
-    }*/
+    }
 }
