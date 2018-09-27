21e2021d7e3eea328160ba3d7c67e04bca189f8d
darong <darongvann@gmail.com>
2018-09-27 16:26:04 +0700

    aaaa

--- src/main/java/com/eracambodia/era/controller/AdminControlller.java
+++
@@ -1,9 +0,0 @@
-package com.eracambodia.era.controller;
-
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
-
-@RestController
-public class AdminControlller {
-    //@GetMapping("/api/admin/login")
-}
